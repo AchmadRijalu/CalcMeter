6979dc75496ac8dfb08d38ec54ae4b9625307bdb
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2024-06-21 14:29:01 +0700

    Initial commit

---
+++ CalcMeter/CalcMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcMeterApp.swift
+//  CalcMeter
+//
+//  Created by Achmad Rijalu on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CalcMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
